b8477dfe9a5901186d20dc9e6f2954396599d87d
Conejosalvaje123 <angelvazquezoriginal@gmail.com>
2024-10-07 10:44:21 -0600

    variables

--- src/app/[hash]/page.tsx
+++ src/app/[hash]/page.tsx
@@ -34,7 +34,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:3000/auth/sign-in", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/sign-in`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
